5e3f9161d7a20ff58d62b4f87366b82999ff8131
hoshikuzuryuu <yaminoiroha@gmail.com>
2024-09-04 10:13:59 -0700

    IM DONE YESSSSSS

--- problem1.java
+++ problem1.java
@@ -1,1 +1,11 @@
-// Create your class here; don't forget the main method
+public class problem1
+{
+    public static void main(String[]args)
+    {
+        
+        String food;
+        food = "calamari";
+        System.out.print("I like to eat " + food +" as well!");
+
+    }
+}
--- problem3.java
+++ problem3.java
@@ -1,1 +1,15 @@
-// Create your class here; don't forget the main method
+public class problem3
+{
+    public static void main(String[]args)
+    {
+        
+        String name;
+        String state;
+
+        name = "Daniel";
+        state = "California";
+
+        System.out.print("My name is " + name + ". I live in " + state + ".");
+
+    }
+}
